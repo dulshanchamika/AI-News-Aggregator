315b2dd59b2eebb28e7d1e914a4757bb8ac9ceb6
Dulshan Chamika <dulshanchamika2@gmail.com>
2026-04-06 23:47:58 +0530

    feat: implement Anthropic scraper and orchestrate data collection pipeline in runner.py

--- app/scrapers/anthropic.py
+++ app/scrapers/anthropic.py
@@ -64,6 +64,6 @@ class AnthropicScraper:
 
 if __name__ == "__main__":
     scraper = AnthropicScraper()
-    articles: List[AnthropicArticle] = scraper.get_articles(hours=100)
+    articles: List[AnthropicArticle] = scraper.get_articles(hours=240)
     markdown: str = scraper.url_to_markdown(articles[1].url)
     print(markdown)
